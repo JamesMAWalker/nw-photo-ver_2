2d257a161bcffba262ad8d4a25c6ba9f07d5a921
James Walker <jamesmawalker@gmail.com>
2020-08-03 15:53:15 +0700

    Remove onClick that routes to fullRes, and all associated styling. Will leave component intact for future use.

--- src/components/1-photo/photo-large.component.jsx
+++ src/components/1-photo/photo-large.component.jsx
@@ -3,39 +3,16 @@ import { withRouter } from 'react-router-dom';
 
 class PhotoLarge extends Component {
 
-  constructor(props) {
-    super(props);
-    
-
-  }
-  
-  
-  adjustCrop = name => {
-    if (name === 'balloons') {
-      return '--50';
-    } else if (name === 'float') {
-      return '--33'
-    }
-  };
-
-  handleClick = (e) => {
-    const { history, alt, catID, photoID } = this.props;
-
-    history.push(`/${catID}/${photoID}`);
-  }
-
   render() {
-    const { alt, src, key, history, catID, photoID } = this.props;
+    const { alt, src, key } = this.props;
     
-    const photoPos = this.adjustCrop(alt);
 
     return (
       <img
-        onClick={this.handleClick}
         key={key}
         src={src}
         alt={alt}
-        className={`photo--large photo--large${photoPos}`}
+        className={`photo--large photo--large`}
       />
     );
   }
