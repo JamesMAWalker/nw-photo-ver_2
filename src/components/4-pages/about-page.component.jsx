acd58d9944c36dbed21c8667da9d68b5408e4767
James Walker <jamesmawalker@gmail.com>
2020-08-04 15:01:37 +0700

    Reconfigure about page to match format of other pages. Styling still needs work.

--- src/components/4-pages/about-page.component.jsx
+++ src/components/4-pages/about-page.component.jsx
@@ -5,6 +5,7 @@ import { ReactComponent as Logo } from '../../Icons/logo-icon.svg';
 import { ReactComponent as Divider } from '../../Icons/title-divider.svg';
 import MenuHandle from '../2-menu/menu-handle.component';
 import MenuPhone from '../2-menu/menu-container--phone.component';
+import MenuContainer from '../2-menu/menu-container.component';
 
 class AboutPage extends Component {
   render() {
@@ -12,31 +13,26 @@ class AboutPage extends Component {
 
     return (
       <>
-        <MenuHandle toggleMenu={toggleMenu} />
-        <MenuPhone toggleMenu={toggleMenu} menuOpen={menuOpen} history={history}/>
-        <div className='about'>
-          <div className='about__info'>
-            <Link to='/home'>
-              <Logo className='about__logo' />
-            </Link>
-            <span className='about__header-section'>
-              <h1 className='about__heading'>About Nishelle</h1>
-              <Divider className='divider' />
-            </span>
-            <div className='about__text'>
-              Nishelle loves to tell stories... through her photography. Her
-              stories are based upon daydreams, reality and often a mixture of
-              the two - depending on how she see's fit. Through her camera she
-              expresses the inner world of both herself and her subjects.
-              Together they make magic!
-            </div>
-          </div>
+      <div className='about'>
+          <MenuContainer/>
           <div className='about__photo-section'>
-            <img
-              src='https://res.cloudinary.com/jameswalker-work/image/upload/f_auto/q_auto/v1588406318/NW_Photography/nisa-flowers_vcgvgc.jpg'
-              alt=''
-              className='about__photo'
-            />
+            <div className='about__text'>
+                <h1 className='about__heading'>
+                  <span className='about__heading--1'>About</span>
+                  <br />
+                  <span className='about__heading--2'>Nishelle</span>
+                </h1>
+                <Divider className='divider' />
+              <span className='about__text--1'>
+                Nishelle loves to tell stories... through her photography.
+              </span>
+              <span className='about__text--2'>
+                Her stories are based upon daydreams, reality and often a
+                mixture of the two - depending on how she see's fit. Through her
+                camera she expresses the inner world of both herself and her
+                subjects. Together they make magic!
+              </span>
+            </div>
           </div>
         </div>
       </>
